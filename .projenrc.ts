7fca4ebf4bcd8e47651c80e84cb4960a8638035f
Yutaro Sakamoto <yutaro-sakamoto@yutaro-sakamoto.com>
2025-02-11 21:30:41 +0900

    feat: add publisher settings

--- .projenrc.ts
+++ .projenrc.ts
@@ -1,12 +1,22 @@
-import { cdk } from 'projen';
+import { cdk, github } from 'projen';
+const repoUrl = 'https://github.com/yutaro-sakamoto/projen-cdk';
 const project = new cdk.JsiiProject({
   author: 'Yutaro Sakamoto',
   authorAddress: 'yutaro-sakamoto@yutaro-sakamoto.com',
   defaultReleaseBranch: 'main',
   jsiiVersion: '~5.7.0',
-  name: 'projen-cdk',
+  name: '@yutaro-sakamoto/projen-cdk',
+  repositoryUrl: repoUrl, // Required for GitHub Packages
+  repository: repoUrl, // Required for GitHub Packages
+  npmRegistryUrl: 'https://npm.pkg.github.com', // Required for GitHub Packages
+  releaseToNpm: true, // Required for GitHub Packages
+  depsUpgradeOptions: {
+    workflowOptions: {
+      projenCredentials:
+        github.GithubCredentials.fromPersonalAccessToken({ secret: 'GITHUB_TOKEN' }),
+    },
+  },
   projenrcTs: true,
-  repositoryUrl: 'https://github.com/yutaro-sakamoto/projen-cdk.git',
 
   deps: ['projen'], /* Runtime dependencies of this module. */
   // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
@@ -15,4 +25,15 @@ const project = new cdk.JsiiProject({
   devDeps: ['projen'],
   peerDeps: ['projen'],
 });
+
+//const publisher = new release.Publisher(project, {
+//  buildJobId: 'my-build-job',
+//  artifactName: 'dist',
+//});
+//
+//publisher.publishToNpm({
+//  registry: 'npm.pkg.github.com',
+//  // also sets npmTokenSecret
+//});
+
 project.synth();
