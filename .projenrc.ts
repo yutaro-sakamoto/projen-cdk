8b7ea7e1957813106d3b53cb88bfe467bc421ffc
Yutaro Sakamoto <yutaro-sakamoto@yutaro-sakamoto.com>
2025-02-12 00:11:39 +0900

    feat: add dependabot settings

--- .projenrc.ts
+++ .projenrc.ts
@@ -18,10 +18,15 @@ const project = new cdk.JsiiProject({
         github.GithubCredentials.fromPersonalAccessToken({ secret: 'GITHUB_TOKEN' }),
     },
   },
+  dependabot: true,
+  dependabotOptions: {
+    scheduleInterval: github.DependabotScheduleInterval.MONTHLY,
+  },
   peerDeps: ['projen'],
   // deps: [],                /* Runtime dependencies of this module. */
   // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
   // devDeps: [],             /* Build dependencies for this module. */
   // packageName: undefined,  /* The "name" in package.json. */
 });
+
 project.synth();
