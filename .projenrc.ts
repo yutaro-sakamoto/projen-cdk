54a7117748415bbee886422a8f46683e93be25cb
Yutaro Sakamoto <yutaro-sakamoto@yutaro-sakamoto.com>
2025-02-11 23:13:34 +0900

    feat: .github/custom-instructions.md

--- .projenrc.ts
+++ .projenrc.ts
@@ -1,13 +1,24 @@
-import { cdk } from 'projen';
+import { cdk, github } from 'projen';
+const repoUrl = 'https://github.com/yutaro-sakamoto/projen-cdk.git';
+
 const project = new cdk.JsiiProject({
   author: 'Yutaro Sakamoto',
   authorAddress: 'yutaro-sakamoto@yutaro-sakamoto.com',
   defaultReleaseBranch: 'main',
   jsiiVersion: '~5.7.0',
-  name: 'projen-cdk',
   projenrcTs: true,
-  repositoryUrl: 'https://github.com/yutaro-sakamoto/projen-cdk.git',
-
+  name: '@yutaro-sakamoto/projen-cdk', // Scope the package to your GitHub username
+  repositoryUrl: repoUrl, // Required for GitHub Packages
+  repository: repoUrl, // Required for GitHub Packages
+  npmRegistryUrl: 'https://npm.pkg.github.com', // Required for GitHub Packages
+  releaseToNpm: true, // Required for GitHub Packages
+  depsUpgradeOptions: {
+    workflowOptions: {
+      projenCredentials:
+        github.GithubCredentials.fromPersonalAccessToken({ secret: 'GITHUB_TOKEN' }),
+    },
+  },
+  peerDeps: ['projen'],
   // deps: [],                /* Runtime dependencies of this module. */
   // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
   // devDeps: [],             /* Build dependencies for this module. */
