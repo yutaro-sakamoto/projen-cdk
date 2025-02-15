e5245cd382e3011c052a7ecd1b8a03d1e100d33a
Yutaro Sakamoto <yutaro-sakamoto@yutaro-sakamoto.com>
2025-02-15 23:14:26 +0900

    add: a generator of PULL_REQUEST_TEMPLATE.md

--- src/awscdk-app-ts.ts
+++ src/awscdk-app-ts.ts
@@ -18,6 +18,42 @@ GitHub Actionsのワークフローファイルにおいて、useキーワード
 `,
     });
 
+    new SampleFile(this, '.github/PULL_REQUEST_TEMPLATE.md', {
+      contents: `# 概要
+
+Pull Requestの目的と概要を記載する
+
+# 変更点
+
+変更点や修正箇所を箇条書きで記載する
+
+- 変更点1
+- 変更点2
+- 変更点3
+
+# 影響範囲
+
+影響範囲について記載すること
+
+# テスト
+
+この修正に対するテスト内容を記載すること
+
+# 関連Issue
+
+このPull Requestに関連するIssueを記載すること
+
+# 関連Pull Request
+
+このPull Requestに関連するPull Requestを記載すること
+
+# その他
+
+その他特筆すべき事項があれば記載する
+
+`,
+    });
+
     new YamlFile(this, '.github/workflows/check-workflows.yml', {
       obj: {
         name: 'Check workflow files',
